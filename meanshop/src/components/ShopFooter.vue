e733d0cb1e2a65670ef0e5683b88d237079f324d
andrronov <andrew270504cska@gmail.com>
2023-02-07 21:35:19 +0300

    update 07.02.2023

---
+++ meanshop/src/components/ShopFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <div class="footer__container">
+      <a href="#" class="link">На главную</a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "ShopFooter",
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/styles/footer_style.scss";
+</style>
